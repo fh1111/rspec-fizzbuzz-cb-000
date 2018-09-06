00a394710bd9ab1b694a30a3ed73336609287208
fh.1111@gmail.com <fh.1111@gmail.com>
2018-09-06 00:03:57 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,16 +4,12 @@
 
 def fizzbuzz (number)
 
-  if (number % 3 == 0)
-    "Fizz"
-  end
-
-  if (number % 5 == 0)
-    "Buzz"
-  end
-
   if (number % 3 == 0 && number % 5 == 0)
     "FizzBuzz"
+  elsif (number % 3 == 0)
+    "Fizz"
+  elsif (number % 5 == 0)
+    "Buzz"
   end
 
 end
